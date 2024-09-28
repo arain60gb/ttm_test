42432a4262633bcf59458ba610231e2280f4d4f9
sherijigarx <155580193+sherijigarx@users.noreply.github.com>
2024-09-28 16:00:29 +0500

    Update protocol.py

--- ttm/protocol.py
+++ ttm/protocol.py
@@ -14,28 +14,28 @@ class MusicGeneration(bt.Synapse, BaseModel):
         validate_assignment = True
         protected_namespaces = ()
 
-        text_input: str = Field(
-            ...,
-            title="Text Input",
-            description="Textual directives or descriptions intended to guide the music generation process."
-        )
-        model_name: Optional[Literal['facebook/musicgen-medium', 'facebook/musicgen-large']] = Field(
-            'facebook/musicgen-medium',
-            title="Model Name",
-            description="The machine learning model employed for music generation. Supported models: "
-                        "'facebook/musicgen-medium', 'facebook/musicgen-large'."
-        )
-        music_output: Optional[List[Any]] = Field(
-            default=None,
-            title="Music Output",
-            description="The resultant music data, encoded as a list of bytes, generated from the text input."
-        )
-        
-        duration: Optional[int] = Field(
-            default=None,
-            title="Duration",
-            description="The length of the generated music piece, specified in seconds."
-        )
+    text_input: str = Field(
+        ...,
+        title="Text Input",
+        description="Textual directives or descriptions intended to guide the music generation process."
+    )
+    model_name: Optional[Literal['facebook/musicgen-medium', 'facebook/musicgen-large']] = Field(
+        'facebook/musicgen-medium',
+        title="Model Name",
+        description="The machine learning model employed for music generation. Supported models: "
+                    "'facebook/musicgen-medium', 'facebook/musicgen-large'."
+    )
+    music_output: Optional[List[Any]] = Field(
+        default=None,
+        title="Music Output",
+        description="The resultant music data, encoded as a list of bytes, generated from the text input."
+    )
+    
+    duration: Optional[int] = Field(
+        default=None,
+        title="Duration",
+        description="The length of the generated music piece, specified in seconds."
+    )
 
 
     def deserialize(self) -> List[bytes]:
